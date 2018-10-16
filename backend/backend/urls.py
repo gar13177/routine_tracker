62fc5e404e492ca55327b00a1ddcb30d071f2a44
Brian Caffey <bcaffey@drydengroup.com>
2018-10-16 17:15:45 -0400

    fixed networking and static files issues

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'Admin Login'
+
 urlpatterns = [
     path('api/', include('posts.urls')),
     path('', include('accounts.urls')),
